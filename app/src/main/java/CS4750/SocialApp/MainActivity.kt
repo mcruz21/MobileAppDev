a3f16236b89d526f7d7857be0a6137d68eeb0c0d
neydachavira <56134733+neydachavira@users.noreply.github.com>
2020-11-23 15:33:08 -0800

    Update MainActivity.kt

--- app/src/main/java/CS4750/SocialApp/MainActivity.kt
+++ app/src/main/java/CS4750/SocialApp/MainActivity.kt
@@ -2,61 +2,76 @@ package CS4750.SocialApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
-
+import android.widget.Button
+import android.widget.TextView
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
-     private val questionViewModel: QuestionViewModel by lazy {
-         ViewModelProviders.of(this).get(QuestionViewModel::class.java)
-     }
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_1, true),
+        Question(R.string.question_2, true),
+        Question(R.string.question_3, true),
+        Question(R.string.question_4, true),
+        Question(R.string.question_5, true),
+        Question(R.string.question_6, true),
+        Question(R.string.question_7, true),
+        Question(R.string.question_8, true),
+        Question(R.string.question_9, true),
+        Question(R.string.question_10, true),
+        Question(R.string.question_11, true),
+        Question(R.string.question_12, true),
+        Question(R.string.question_13, true),
+        Question(R.string.question_14, true),
+        Question(R.string.question_15, true)
+    )
+
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+
     }
-
-    override fun onActivityResult(){};
-
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart() called")
     }
+
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume() called")
     }
+
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause() called")
     }
 
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        Log.i(TAG,"onSaveInstanceState")
-    }
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop() called")
     }
-    override fun onDestroy(){
+
+    override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG,"onDestroy() called")
-    }
-    private fun updateQuestion() {
-        XButton.isEnabled = true
-        YalseButton.isEnabled= true
-        val questionTextResId = questionViewModel.currentQuestionText
-        questionTextView.setText(questionTextResId)
-    }
-    private fun updateQuestion1() {
-        XButton.isEnabled = false
-        YButton.isEnabled= false
-        val questionTextResId = questionViewModel.currentQuestionText
-        questionTextView.setText(questionTextResId)
-    }
-    companion object {
-        private const val TAG = "MainActivity"
-        private const val KEY_INDEX = "index"
+        Log.d(TAG, "onDestroy() called")
     }
 
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
 }
