b8343291fcb4c68453c9c10d84b85756ffe02813
neydachavira <56134733+neydachavira@users.noreply.github.com>
2020-12-03 15:01:02 -0800

    Update MainActivity.kt

--- app/src/main/java/CS4750/SocialApp/MainActivity.kt
+++ app/src/main/java/CS4750/SocialApp/MainActivity.kt
@@ -1,8 +1,5 @@
-package com.bignerdranch.android.geoquiz
+package CS4750.SocialApp
 
-
-import CS4750.SocialApp.Question
-import CS4750.SocialApp.QuizViewModel
 import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -13,86 +10,63 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-private const val REQUEST_CODE_CHEAT = 0
+
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
-    private lateinit var prevButton: ImageButton
+
+    private lateinit var nextButton: ImageButton
+    //private lateinit var passButton: ImageButton
+    private lateinit var failButton: ImageButton
     private lateinit var questionTextView: TextView
 
 
-    private val questionViewModel: QuizViewModel by lazy {
+
+    private val quizViewModel: QuizViewModel by lazy {
         ViewModelProviders.of(this).get(QuizViewModel::class.java)
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(Companion.TAG, "onCreate(Bundle?) called")
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
-        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
-        questionViewModel.currentIndex = currentIndex
-
         nextButton = findViewById(R.id.next_button)
-
         questionTextView = findViewById(R.id.question_text_view)
-
         nextButton.setOnClickListener {
-            questionViewModel.moveToNext()
+            quizViewModel.moveToNext()
             updateQuestion()
         }
         updateQuestion()
 
     }
-    override fun onActivityResult(requestCode: Int,
-                                  resultCode: Int,
-                                  data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode != Activity.RESULT_OK) {
-            return
-        }
-    }
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart() called")
     }
+
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "onResume() called")
     }
+
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause() called")
     }
-    override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        super.onSaveInstanceState(savedInstanceState)
-        Log.i(TAG, "onSaveInstanceState")
-        savedInstanceState.putInt(KEY_INDEX, questionViewModel.currentIndex)
-    }
+
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop() called")
     }
+
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy() called")
     }
+
     private fun updateQuestion() {
-        val questionTextResId = questionViewModel.currentQuestionText
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
-    }
-    private fun updateQuestion1() {
-        trueButton.isEnabled = false
-        falseButton.isEnabled= false
-        val questionTextResId = questionViewModel.currentQuestionText
-        questionTextView.setText(questionTextResId)
-    }
-
-
-    companion object {
-        private const val TAG = "MainActivity"
-        private const val KEY_INDEX = "index"
     }
 }
