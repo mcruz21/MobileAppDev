5a9127af0a6c4152de5f015b647fc6b1bb5a8ec0
Cruz <cruzmax21@gmail.com>
2020-12-05 17:04:14 -0800

    homepage added

--- app/src/main/java/CS4750/SocialApp/MainActivity.kt
+++ app/src/main/java/CS4750/SocialApp/MainActivity.kt
@@ -1,123 +1,31 @@
 package com.bignerdranch.android.geoquiz
 
-
-import CS4750.SocialApp.QuizViewModel
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
-import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProviders
 
 private const val REQUEST_CODE_CHEAT = 0
 class MainActivity : AppCompatActivity() {
-    private lateinit var nextButton: ImageButton
-    //private lateinit var passButton: ImageButton
-    private lateinit var failButton: ImageButton
-    private lateinit var questionTextView: TextView
-    private var counter =0;
-    private var counter1 =0;
-
-
-
-
-    private val questionViewModel: QuizViewModel by lazy {
-        ViewModelProviders.of(this).get(QuizViewModel::class.java)
-    }
+    private lateinit var playButton: Button
+    private lateinit var ruleButton: Button
+    private lateinit var homeTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(Companion.TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
-        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
-        questionViewModel.currentIndex = currentIndex
-
-        nextButton = findViewById(R.id.next_button)
-
-        questionTextView = findViewById(R.id.question_text_view)
-        nextButton.setOnClickListener {
-            questionViewModel.moveToNext()
-            updateQuestion()
+        homeTextView = findViewById(R.id.home)
+        playButton = findViewById(R.id.playbutton)
+        ruleButton = findViewById(R.id.rulebutton)
+        playButton.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
         }
-        updateQuestion()
 
     }
-    private fun startTimeCounter() {
-        val countTime: TextView = findViewById(R.id.countTime)
-        object : CountDownTimer(5000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                countTime.text = counter.toString()
-                counter++
-                nextButton.setOnClickListener {
-                    this.cancel()
-                    questionViewModel.moveToNext()
-                    updateQuestion()
-                }
-            }
-
-
-            override fun onFinish() {
-                countTime.text = "Times up!"
-            }
-        }.start()
-    }
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode != Activity.RESULT_OK) {
-            return
-        }
-    }
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart() called")
-    }
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume() called")
-    }
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause() called")
-    }
-    override fun onSaveInstanceState(savedInstanceState: Bundle) {
-        super.onSaveInstanceState(savedInstanceState)
-        Log.i(TAG, "onSaveInstanceState")
-        savedInstanceState.putInt(KEY_INDEX, questionViewModel.currentIndex)
-    }
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop() called")
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy() called")
-    }
-    private fun updateQuestion() {
-        counter =0
-        startTimeCounter()
-        val questionTextResId = questionViewModel.currentQuestionText
-        questionTextView.setText(questionTextResId)
-
-
-    }
-    private fun updateQuestion1() {
-        val questionTextResId = questionViewModel.currentQuestionText
-        questionTextView.setText(questionTextResId)
-    }
-
-
-
-    companion object {
-        private const val TAG = "MainActivity"
-        private const val KEY_INDEX = "index"
-    }
+    
 }
